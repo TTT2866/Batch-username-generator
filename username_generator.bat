a820d8b237c770085a03dff56b5f5ffe7ec38730
TTT2866 <41979926+TTT2866@users.noreply.github.com>
2019-02-10 00:19:29 +1100

    Main file

---
+++ username_generator.bat
@@ -0,0 +1,140 @@
+@echo off
+color f
+cd %~dp0"
+::==============================================
+::Optional variables..
+set LogUsernameRawToFile=true
+::Will log the raw username with the md5 hash to the database file
+::==============================================
+
+if not exist database.txt echo Creating database... & call :databasecreate
+cls
+if not exist wordlist.txt echo Downloading wordlist... & call :downloadbyhackoo https://raw.githubusercontent.com/TTT2866/Batch-username-generator/master/wordlist.txt wordlist.txt
+cls
+if not exist md5.bat echo Downloading md5.bat... & call :downloadbyhackoo https://raw.githubusercontent.com/TTT2866/Batch-username-generator/master/md5.bat md5.bat
+cls
+
+:start
+cls
+echo.
+echo Generating your username..
+echo.
+call :func
+set word1=%random_word%
+call :cap
+set word1=%word1x%
+call :func
+set word2=%random_word%
+
+set /a "randomnum=%random% %% 1000"
+set username=%word1%%word2%%randomnum%
+
+echo %username% >temp.txt
+
+call :md5func
+
+set database=database.txt
+for /F "delims=" %%a in ('findstr /I /N "%md5%" %database%') do set "datalookup=%%a"
+if "%datalookup%"=="" (goto next) else goto start
+:next
+echo %username%
+echo %md5%
+if %LogUsernameRawToFile%==true (echo %md5% ^| %username% >>database.txt) else (echo %md5% ^| >>database.txt)
+
+echo.
+echo Done.
+echo Press any key to exit
+pause >nul
+exit
+
+:func
+set "file=wordlist.txt"
+for /f %%# in (
+    'findstr  /r /n "^" "%file%" ^|find /c ":"'
+) do (
+    set lines=%%#
+)
+set /a random_line=random_line=%random% %% %lines%
+if random_line==0 (
+    set "skip="
+) else (
+    set "skip=skip=%random_line%"
+)
+
+for /f "usebackq %skip%" %%# in ("%file%") do (
+    set "random_word=%%#"
+    goto :break
+)   
+:break
+endlocal
+goto :EOF
+
+:md5func
+
+set "file1=temp.txt"
+call md5.bat "%file1%" md5
+del temp.txt
+goto :EOF
+
+:cap
+set x=%word1:~-0,1%
+if %x%==a set firstcap=A
+if %x%==b set firstcap=B
+if %x%==c set firstcap=C
+if %x%==d set firstcap=D
+if %x%==e set firstcap=E
+if %x%==f set firstcap=F
+if %x%==g set firstcap=G
+if %x%==h set firstcap=H
+if %x%==i set firstcap=I
+if %x%==j set firstcap=J
+if %x%==k set firstcap=K
+if %x%==l set firstcap=L
+if %x%==m set firstcap=M
+if %x%==n set firstcap=N
+if %x%==o set firstcap=O
+if %x%==p set firstcap=P
+if %x%==q set firstcap=Q
+if %x%==r set firstcap=R
+if %x%==s set firstcap=S
+if %x%==t set firstcap=T
+if %x%==u set firstcap=U
+if %x%==v set firstcap=V
+if %x%==w set firstcap=W
+if %x%==x set firstcap=X
+if %x%==y set firstcap=Y
+if %x%==z set firstcap=Z
+set word1temp=%word1:~1%
+set word1x=%firstcap%%word1temp%
+goto :EOF
+
+:databasecreate
+echo ================================================================ >database.txt
+echo ^|      MD5 hash of username      ^|           Username          ^| >>database.txt
+echo ================================================================ >>database.txt
+
+goto :EOF
+
+:downloadbyhackoo
+Ping google.com -n 1 -w 1000
+cls
+if %errorlevel%==1 (set internet=nconnected) else (set internet=connected)
+if %internet%==%nconnected% (echo Error.. you must be connected to internet to download the files required for this script to run. & pause >nul & exit)
+
+::https://stackoverflow.com/questions/48860214/
+Set "url=%~1%"
+for %%# in (%url%) do ( set "File=%tmp%\%%~n#.txt" )
+Call :Download "%url%" "%File%"
+If exist "%File%" ( 
+    ( Type "%File%")>con
+Rem to save the contents in new text file
+( Type "%File%" > %~2%)
+)
+
+::*********************************************************************************
+:Download <url> <File>
+Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%1','%2')"
+::*********************************************************************************
+goto :EOF
+
+
